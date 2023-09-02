9ff031655ae7eae63203fc4644282ca68bd6c6ac
RomanProkhorovich <pomaprokhorovich583@gmail.com>
2023-09-02 21:41:48 +0400

    02.09

--- src/main/java/com/example/schedule/service/GroupService.java
+++ src/main/java/com/example/schedule/service/GroupService.java
@@ -5,5 +5,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GroupService {
+    private final GroupRepository groupRepository;
 
+    public GroupService(GroupRepository groupRepository) {
+        this.groupRepository = groupRepository;
+    }
+
+    public
 }
